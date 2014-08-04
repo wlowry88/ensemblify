bcb46d7aee5297a22cd21d8eeda40115991505bc
Will Lowry <william.c.lowry@gmail.com>
2014-08-04 18:22:46 -0400

    adds the request creation action based on invite button in user show

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,18 @@
 class RequestsController < ApplicationController
+
+	def create
+
+		@request = Request.new(request_params)
+		if @request.save
+			redirect_to Group.find(@request.group_id)
+		end
+		
+
+	end
+
+
+	private
+		def request_params
+			params.require(:request).permit(:user_approved, :group_approved, :user_id, :group_id, :finalized, :part)
+		end
 end

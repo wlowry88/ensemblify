7ee43033efcd78a154e55e1894b839aa9437b6e6
Will Lowry <william.c.lowry@gmail.com>
2014-08-10 16:32:49 -0400

    fixes omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,12 @@
+  	if ENV['RAILS_ENV'] == 'production'
+  		HOST= 'ensemblify.herokuapp.com'
+    else
+    	HOST= 'localhost:3000'
+    end
+
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_LOCAL_ID'], ENV['FACEBOOK_LOCAL_SECRET'], {
-  	if ENV['RAILS_ENV'] == 'production'
-  		callback_url: 'http://ensemblify.herokuapp.com/auth/facebook/callback'
-    else
-    	callback_url: 'http://localhost:3000/auth/facebook/callback'
-    end
+  	callback_url: "http://#{HOST}/auth/facebook/callback"
   }
 end

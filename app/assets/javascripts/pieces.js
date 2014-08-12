1291a5cd1a8d443ccccabb98e78ca54238e421e1
Will Lowry <william.c.lowry@gmail.com>
2014-08-11 20:25:13 -0400

    pieces index search works

--- app/assets/javascripts/pieces.js
+++ app/assets/javascripts/pieces.js
@@ -8,24 +8,24 @@ $(function(){
 		var $name = $("#piece_name").next().find("span")
 		var $composer = $("#piece_composer").next().find("span")
 		var $ensemble_type = $("#piece_instrumentation").next().find("span")
-		debugger;
-		if($name.text()!=("Select an Option"){
+
+		if($name.text()!=("Select an Option")){
 			$.each($(".piece_name_field"), function(i, value){
 				if($(value).text() != $name.text()){
 					$(value).parent().hide();
 				}
 			});
 		}
-		if($composer.text()!=("Select an Option"){
+		if($composer.text()!=("Select an Option")){
 			$.each($(".composer_name_field"), function(i, value){
-				if($(value).text() != $composer){
+				if($(value).text() != $composer.text()){
 					$(value).parent().hide();
 				}
 			});
 		}
-		if($ensemble_type.text()!=("Select an Option"){
+		if($ensemble_type.text()!=("Select an Option")){
 			$.each($(".interests_field"), function(i, value){
-				if($(value).text() !== $ensemble_type){
+				if($(value).text() !== $ensemble_type.text()){
 					$(value).parent().hide();
 				}
 			});
